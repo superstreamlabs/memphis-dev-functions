5490de8d6e63cb5425a19fc5a82df6c0985981c5
Shay Bratslavsky <shay@memphis.dev>
2023-11-22 21:41:32 +0200

    Update main.go remove shay test

--- flatten_json/main.go
+++ flatten_json/main.go
@@ -46,6 +46,5 @@ func FlattenMessages(message []byte, headers map[string]string, inputs map[strin
 }
 
 func main() {
-	fmt.Println("hello world") // test by shay - will remove soon
 	memphis.CreateFunction(FlattenMessages)
 }
