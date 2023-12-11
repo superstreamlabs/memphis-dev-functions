4f52afedf15df15b3bf876e7059ecafc0afee810
Idan Asulin <74712806+idanasulinmemphis@users.noreply.github.com>
2023-12-11 12:13:17 +0200

    Update index.js

--- hello-world-nodejs/index.js
+++ hello-world-nodejs/index.js
@@ -1,16 +1,9 @@
 const { memphis } = require("memphis-functions");
 
-exports.handler = async (event) => { // The name of this file and this function should match the handler field in the memphis.yaml file in the following format <file name>.<function name>
+exports.handler = async (event) => {
     return await memphis.createFunction(event, eventHandler, asJson = true);
 };
 
-/**
- * https://github.com/memphisdev/memphis.js/tree/functions_wrapper#creating-a-memphis-function
- * @param {Uint8Array} payload 
- * @param {Object} headers 
- * @param {Object} inputs 
- * @returns {Object} 
- */
 function eventHandler(payload, headers, inputs) {
     payload[inputs.field_to_ingest] = "Hello from Memphis!";
 
