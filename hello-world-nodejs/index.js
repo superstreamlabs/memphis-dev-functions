e65afc50f9e3b799c2d64c78b4cdfcdb9f21077e
idanasulinStrech <idan@memphis.dev>
2023-12-11 11:30:40 +0200

    update the hello world functions

--- hello-world-nodejs/index.js
+++ hello-world-nodejs/index.js
@@ -1,7 +1,7 @@
 const { memphis } = require("memphis-functions");
 
 exports.handler = async (event) => { // The name of this file and this function should match the handler field in the memphis.yaml file in the following format <file name>.<function name>
-    return await memphis.createFunction(event, eventHandler);
+    return await memphis.createFunction(event, eventHandler, asJson = true);
 };
 
 /**
@@ -12,15 +12,10 @@ exports.handler = async (event) => { // The name of this file and this function 
  * @returns {Object} 
  */
 function eventHandler(payload, headers, inputs) {
-    // Handle event here
-    // Here is a short example of converting the payload to a json object and returning it as an Uint8Array
-    const decodedPayload = payload.toString('utf-8');
-    const asJson = JSON.parse(decodedPayload);
-
-    asJson[inputs.field_to_ingest] = "Hello from Memphis!";
+    payload[inputs.field_to_ingest] = "Hello from Memphis!";
 
     return {
-        processedMessage: Buffer.from(JSON.stringify(asJson), 'utf-8'),
+        processedMessage: payload,
         processedHeaders: headers
     };
 }
